8cc4ff5b900aec73a4b1aa7621ee65a44b08d274
Krishna <ramain77@gmail.com>
2021-03-11 13:46:45 +0700

    package: remove vue-axios

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/tailwind.css";
-import VueAxios from "vue-axios";
-import axios from "axios";
 import ApiService from "./services/api";
 
 ApiService.init(process.env.VUE_APP_API_ORIGIN);
 
 createApp(App)
   .use(router)
-  .use(VueAxios, axios)
   .mount("#app");
