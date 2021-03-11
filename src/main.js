c3362d67b6c5dbadc74fda104d4f12a78c2af067
Krishna <ramain77@gmail.com>
2021-03-11 14:03:19 +0700

    feat(mixin): add titleMixin config

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/tailwind.css";
 import ApiService from "./services/api";
+import titleMixin from "./mixins/titleMixin";
 
 ApiService.init(process.env.VUE_APP_API_ORIGIN);
 
 createApp(App)
   .use(router)
+  .mixin(titleMixin)
   .mount("#app");
