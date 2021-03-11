af3ee6b34cf9314b55fb13c63023ec6f62806c3c
Krishna <ramain77@gmail.com>
2021-03-11 13:39:59 +0700

    add Content-Type application/json

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -9,6 +9,7 @@ const ApiService = {
   },
   setHeader() {
     axios.defaults.headers.common["Accept"] = "application/json";
+    axios.defaults.headers.common["Content-Type"] = "application/json";
   },
   get(resource) {
     return axios.get(resource);
