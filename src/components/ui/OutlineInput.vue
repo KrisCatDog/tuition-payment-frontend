a4c55d03a98fd75513ea030d77026de2fe9ffcf2
Krishna <ramain77@gmail.com>
2021-03-12 14:26:36 +0700

    feat(components): add main ui components

---
+++ src/components/ui/OutlineInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <input
+    :type="type"
+    class="mt-2 block w-full font-medium px-3 border-0 border-b-2 rounded border-gray-400 focus:bg-green-50 focus:ring-0 focus:border-green-400"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :placeholder="placeholder"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
