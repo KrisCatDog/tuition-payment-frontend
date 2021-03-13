50a91b6952644b1dfa1fa64a7b67004da61d7f67
Krishna <ramain77@gmail.com>
2021-03-13 15:15:10 +0700

    feat(components): add outline textarea

---
+++ src/components/ui/OutlineTextarea.vue
@@ -0,0 +1,16 @@
+<template>
+  <textarea
+    class="mt-1 block w-full font-medium px-2 border-0 border-b-2 rounded-tl rounded-tr border-gray-400 focus:bg-green-50 focus:ring-0 focus:border-green-400"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: "AppInput",
+  props: {
+    modelValue: String,
+  },
+};
+</script>
