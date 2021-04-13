d7c15cf3bc2016c74e6602bb415343e2afa12d06
Krishna <ramain77@gmail.com>
2021-04-13 10:08:58 +0700

    feat(home): implement get home & add dynamic cards

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import { createStore } from "vuex";
+import home from "./modules/home";
 
 export default createStore({
   state: {},
-  mutations: {},
   actions: {},
-  modules: {},
+  mutations: {},
+  modules: {
+    home,
+  },
 });
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,104 +6,50 @@
       Home
     </h2>
 
-    <!-- Cards -->
-    <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-      <!-- Card -->
-      <div
-        class="flex items-center p-4 bg-white rounded-lg shadow-sm dark:bg-gray-800"
-      >
-        <div
-          class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500"
-        >
-          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path
-              d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
-            ></path>
-          </svg>
-        </div>
-        <div>
-          <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-            Total clients
-          </p>
-          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-            6389
-          </p>
-        </div>
-      </div>
-      <!-- Card -->
-      <div
-        class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800"
-      >
-        <div
-          class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500"
-        >
-          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path
-              fill-rule="evenodd"
-              d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </div>
-        <div>
-          <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-            Account balance
-          </p>
-          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-            $ 46,760.89
-          </p>
-        </div>
-      </div>
-      <!-- Card -->
-      <div
-        class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800"
-      >
-        <div
-          class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500"
-        >
-          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path
-              d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
-            ></path>
-          </svg>
-        </div>
-        <div>
-          <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-            New sales
-          </p>
-          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-            376
-          </p>
-        </div>
-      </div>
-      <!-- Card -->
-      <div
-        class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800"
-      >
-        <div
-          class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full dark:text-teal-100 dark:bg-teal-500"
-        >
-          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path
-              fill-rule="evenodd"
-              d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </div>
-        <div>
-          <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
-            Pending contacts
-          </p>
-          <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
-            35
-          </p>
-        </div>
-      </div>
-    </div>
+    <Suspense>
+      <template #default> <TheCards /> </template>
+      <template #fallback>
+        <div class="grid gap-4 mb-8 md:grid-cols-2 xl:grid-cols-3">
+          <div
+            class="shadow-sm rounded-md p-4 max-w-sm w-full mx-auto bg-white"
+          >
+            <div class="animate-pulse flex items-center space-x-4">
+              <div class="rounded-full bg-green-400 h-12 w-12"></div>
+              <div class="flex-1 space-y-4 py-1">
+                <div class="h-4 bg-green-400 rounded w-3/4"></div>
+                <div class="h-4 bg-green-400 rounded w-1/2"></div>
+              </div>
+            </div>
+          </div>
 
-    <!-- New Table -->
-    <div class="w-full overflow-hidden rounded-lg shadow-xs">
+          <div
+            class="shadow-sm rounded-md p-4 max-w-sm w-full mx-auto bg-white"
+          >
+            <div class="animate-pulse flex items-center space-x-4">
+              <div class="rounded-full bg-green-400 h-12 w-12"></div>
+              <div class="flex-1 space-y-4 py-1">
+                <div class="h-4 bg-green-400 rounded w-3/4"></div>
+                <div class="h-4 bg-green-400 rounded w-1/2"></div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="shadow-sm rounded-md p-4 max-w-sm w-full mx-auto bg-white"
+          >
+            <div class="animate-pulse flex items-center space-x-4">
+              <div class="rounded-full bg-green-400 h-12 w-12"></div>
+              <div class="flex-1 space-y-4 py-1">
+                <div class="h-4 bg-green-400 rounded w-3/4"></div>
+                <div class="h-4 bg-green-400 rounded w-1/2"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </Suspense>
+
+    <!-- <div class="w-full overflow-hidden rounded-lg shadow-sm">
       <div class="w-full overflow-x-auto">
         <table class="w-full whitespace-no-wrap">
           <thead>
@@ -122,7 +68,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -159,7 +104,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -185,7 +129,7 @@
               <td class="px-4 py-3 text-sm">$ 369.95</td>
               <td class="px-4 py-3 text-xs">
                 <span
-                  class="px-2 py-1 font-semibold leading-tight text-orange-700 bg-orange-100 rounded-full dark:text-white dark:bg-orange-600"
+                  class="px-2 py-1 font-semibold leading-tight text-yellow-700 bg-yellow-100 rounded-full dark:text-white dark:bg-yellow-600"
                 >
                   Pending
                 </span>
@@ -196,7 +140,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -233,7 +176,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -270,7 +212,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -307,7 +248,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -344,7 +284,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -381,7 +320,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -418,7 +356,6 @@
             <tr class="text-gray-700 dark:text-gray-400">
               <td class="px-4 py-3">
                 <div class="flex items-center text-sm">
-                  <!-- Avatar with inset shadow -->
                   <div
                     class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
                   >
@@ -459,7 +396,6 @@
       >
         <span class="flex items-center col-span-3"> Showing 21-30 of 100 </span>
         <span class="col-span-2"></span>
-        <!-- Pagination -->
         <span class="flex col-span-4 mt-2 sm:mt-auto sm:justify-end">
           <nav aria-label="Table navigation">
             <ul class="inline-flex items-center">
@@ -548,14 +484,16 @@
           </nav>
         </span>
       </div>
-    </div>
+    </div> -->
 
     <!-- Charts -->
-    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-      Charts
+    <h2
+      class="my-6 font-mulish text-2xl font-semibold text-gray-700 dark:text-gray-200"
+    >
+      Statistik
     </h2>
     <div class="grid gap-6 mb-8 md:grid-cols-2">
-      <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+      <div class="min-w-0 p-4 bg-white rounded-lg shadow-sm dark:bg-gray-800">
         <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
           Revenue
         </h4>
@@ -572,7 +510,7 @@
           </div>
           <div class="flex items-center">
             <span
-              class="inline-block w-3 h-3 mr-1 bg-teal-600 rounded-full"
+              class="inline-block w-3 h-3 mr-1 bg-red-600 rounded-full"
             ></span>
             <span>Shoes</span>
           </div>
@@ -584,7 +522,7 @@
           </div>
         </div>
       </div>
-      <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+      <div class="min-w-0 p-4 bg-white rounded-lg shadow-sm dark:bg-gray-800">
         <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
           Traffic
         </h4>
@@ -595,7 +533,7 @@
           <!-- Chart legend -->
           <div class="flex items-center">
             <span
-              class="inline-block w-3 h-3 mr-1 bg-teal-600 rounded-full"
+              class="inline-block w-3 h-3 mr-1 bg-red-600 rounded-full"
             ></span>
             <span>Organic</span>
           </div>
@@ -613,8 +551,10 @@
 
 <script>
 import AppLayout from "@/components/layouts/AppLayout";
+import TheCards from "@/components/home/TheCards";
+
 export default {
-  components: { AppLayout },
+  components: { AppLayout, TheCards },
   title: "Home",
 };
 </script>
