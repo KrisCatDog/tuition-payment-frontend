272258c1be53b5c93045e95875f585fb78d99920
Krishna <ramain77@gmail.com>
2021-04-16 09:38:46 +0700

    fix: modify payment report styles

--- src/views/PaymentReport/index.vue
+++ src/views/PaymentReport/index.vue
@@ -4,33 +4,51 @@
       Laporan Pembayaran
     </h2>
 
-    <div class="mb-8 p-5 rounded-3xl shadow-xl bg-white dark:bg-gray-700">
-      <div class="flex space-x-4">
-        <InputGroup class="w-1/2">
-          <TheLabel label="Dari Tanggal" target="start-date" />
-          <input
-            v-model="formData.start_date"
-            id="start-date"
-            type="date"
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-          />
-        </InputGroup>
-        <InputGroup class="w-1/2">
-          <TheLabel label="Sampai Tanggal" target="end-date" />
-          <input
-            v-model="formData.end_date"
-            id="end-date"
-            type="date"
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-          />
-        </InputGroup>
+    <div class="mb-8 bg-white shadow overflow-hidden sm:rounded-lg">
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+          Generate Laporan
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">
+          Halaman generate laporan pembayaran SPP.
+        </p>
       </div>
+      <div class="border-t border-gray-200">
+        <dl>
+          <div
+            class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm font-medium text-gray-500">Dari Tanggal</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <input
+                v-model="formData.start_date"
+                id="start-date"
+                type="date"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+              />
+            </dd>
+          </div>
+          <div
+            class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+          >
+            <dt class="text-sm font-medium text-gray-500">Sampai Tanggal</dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <input
+                v-model="formData.end_date"
+                id="end-date"
+                type="date"
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+              />
+            </dd>
+          </div>
+        </dl>
 
-      <div class="flex justify-end space-x-2">
-        <a :href="exportURL">
-          <BasicButton type="button">Export Excel</BasicButton>
-        </a>
-        <BasicButton type="button">Export PDF</BasicButton>
+        <div class="flex justify-end m-5 space-x-2">
+          <a :href="exportURL">
+            <BasicButton type="button">Export Excel</BasicButton>
+          </a>
+          <BasicButton type="button">Export PDF</BasicButton>
+        </div>
       </div>
     </div>
   </AppLayout>
@@ -40,16 +58,12 @@
 import { computed, onMounted, reactive, ref } from "vue";
 import AppLayout from "@/components/layouts/AppLayout";
 import usePayment from "@/composables/usePayment";
-import InputGroup from "@/components/ui/InputGroup";
-import TheLabel from "@/components/ui/TheLabel";
 import BasicButton from "@/components/ui/BasicButton.vue";
 
 export default {
   title: "Laporan Pembayaran",
   components: {
     AppLayout,
-    TheLabel,
-    InputGroup,
     BasicButton,
   },
   setup() {
