46847bb142a1aef94ece8524f3c2c4b8734413c4
Krishna <ramain77@gmail.com>
2021-04-20 21:34:22 +0700

    fix: change icon

--- src/components/ui/MobileSidebar.vue
+++ src/components/ui/MobileSidebar.vue
@@ -142,19 +142,11 @@
               aria-haspopup="true"
             >
               <span class="inline-flex items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="w-5 h-5"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <line x1="12" y1="1" x2="12" y2="23"></line>
+                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                   <path
-                    d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"
+                    fill-rule="evenodd"
+                    d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+                    clip-rule="evenodd"
                   ></path>
                 </svg>
                 <span class="ml-4">Pembayaran</span>
